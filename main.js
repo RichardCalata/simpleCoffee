0889ddfa130862220a375af5b3bbf5b3cb73558f
richardcalata <calatar6@gmail.com>
2019-08-15 15:57:36 -0500

    added index html, main.js and styles.css

---
+++ main.js
@@ -0,0 +1,8 @@
+
+function myFunction(){
+
+    let input = document.getElementById("myInput");
+    let filter = input.value.toUpperCase();
+    let menuItem = document.getElementById("#coffee-menu");
+    console.log("hello world");
+}
